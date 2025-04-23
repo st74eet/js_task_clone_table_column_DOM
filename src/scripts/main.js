60cbeafa5e01fb8221439006638f260ca33312e7
Iryna Shevkun <sevkunirina65@gmail.com>
2025-04-23 22:15:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const trList = [...document.querySelectorAll('tr')];
+
+trList.forEach((tr) => {
+  const copy = tr.cells[1].cloneNode(true);
+
+  tr.cells[tr.children.length - 2].insertAdjacentElement('afterend', copy);
+});
